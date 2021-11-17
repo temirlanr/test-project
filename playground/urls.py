f7e9b5a08e979477ebc02c856a3fc38215d75f88
Temirlan Rsanov <86775981+temirlanr@users.noreply.github.com>
2021-11-17 20:43:06 +0600

    dont know what im doing....

--- playground/urls.py
+++ playground/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
 from . import views
 
 # URLConf
 urlpatterns = [
-    path('check/', views.first_func)
+    path('check/', views.first_func),
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,8 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from rest_framework import serializers
+from rest_framework.parsers import JSONParser
 
 def first_func(request):
-    x=1
-    y=2
     return render(request, 'test.html', {'name': 'Temirlan'})
